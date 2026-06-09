2c12fb9b6df09172ee782075fdc857436babea91
maxturnr <120778025+maxturnr@users.noreply.github.com>
2026-06-09 07:18:58 +0000

    Switch to new Supabase project (thg-pitch)

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hnypmigzwfavwcwarmnk.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhueXBtaWd6d2Zhdndjd2FybW5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxMDU3MjcsImV4cCI6MjA5MjY4MTcyN30.uRC82cHVPLmDmUe-QKDyEPF8pfpnaMwSNXwfwO-qFBQ';
+const supabaseUrl = 'https://gacldnadnyekeyrinxpp.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhY2xkbmFkbnlla2V5cmlueHBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5ODQzMDUsImV4cCI6MjA5NjU2MDMwNX0.ncw_s4DAECcl48KlPCJ52wpAb834dnOSsTNOqalVee8';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
